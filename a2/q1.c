22220fd5607097737f1fc690631c398627d87321
gumgl <guillaume@labran.ch>
2015-09-26 20:18:02 -0400

    /a2: Complete Q1

--- a2/q1.c
+++ a2/q1.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+/* Guillaume Labranche 260585371
+ * COMP 350 Fall 2015, McGill University
+ * Assignment 1, Question 1 */
 
+
+/* // I didn't see that the questions was asking about integers...
 void q1float() {
 	float x = 1.0f;
 	
@@ -13,7 +18,6 @@ void q1float() {
 	
 	printf("x (float) = %E\n", x);
 }
-
 void q1double() {
 	double x = 1.0f;
 	
@@ -26,6 +30,31 @@ void q1double() {
 	}
 	
 	printf("x (double) = %E\n", x);
+}*/
+
+void q1float() {
+	int i;
+	float x, div;
+	for (i=1; ; i=i+1) {
+		x = (float) i;
+		div = 1 / x;
+		if (div * x != 1.0f)
+			break;
+	}
+	
+	printf("x (float) = %i\n", (int)x);
+}
+void q1double() {
+	int i;
+	double x, div;
+	for (i=1; ; i=i+1) {
+		x = (float) i;
+		div = 1 / x;
+		if (div * x != 1.0f)
+			break;
+	}
+	
+	printf("x (double) = %i\n", (int)x);
 }
 
 int main() {
