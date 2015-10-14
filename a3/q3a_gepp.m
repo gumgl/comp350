57f447e25570fc5d060bafb03d32c2ad8030df20
Guillaume <gumgl@users.noreply.github.com>
2015-10-14 15:40:46 -0400

    /a3: Complete q3a GEPP

--- a3/q3a_gepp.m
+++ a3/q3a_gepp.m
@@ -1,15 +1,29 @@
 % Guillaume Labranche 260585371 COMP-350 Fall 2015
-function xt = q3a_genp(d,a,b)
+function xt = q3a_gepp(d,a,b)
 % q3a_genp.m    Gauss elimination with partial pivoting
 % 
 % input:  d, a, b are 2n+1 x 1 vectors
 % output: x is the solution of Ax=b.
 %
 n = (length(b)-1)/2;
-%fprintf('GENP n=%d\n',n);
+%fprintf('GEPP n=%d\n',n);
 for i = 1:n
     % j: equivalent index on the other side of matrix
     j = 2*n+2-i;
+    if a(i) > d(i)
+        % swap a(i) d(i)
+        tmp = a(i);
+        a(i) = d(i);
+        d(i) = tmp;
+        % swap a(j) d(j)
+        tmp = a(j);
+        a(j) = d(j);
+        d(j) = tmp;
+        % swap b(i) b(j)
+        tmp = b(i);
+        b(i) = b(j);
+        b(j) = tmp;
+    end
     mult = a(i)/d(i);
     d(j) = d(j) - mult*a(j);
     b(j) = b(j) - mult*b(i);
